03ee0be7b2dfdd997c747ed5dfd58c10a00bd3e6
Max Wolffe <max.alan.wolffe@gmail.com>
2015-01-13 23:14:31 -0800

    add menu navigation

--- TEDxBerkeley/app/src/main/java/org/tedxberkeley/www/tedxberkeley/MainActivity.java
+++ TEDxBerkeley/app/src/main/java/org/tedxberkeley/www/tedxberkeley/MainActivity.java
@@ -9,21 +9,28 @@ import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.net.Uri;
 import android.view.View;
 
+import org.tedxberkeley.www.tedxberkeley.About.AboutFragment;
+import org.tedxberkeley.www.tedxberkeley.Schedule.ScheduleFragment;
 import org.tedxberkeley.www.tedxberkeley.Speakers.SpeakerListFragment;
 import org.tedxberkeley.www.tedxberkeley.Views.SlidingTabLayout;
 import org.tedxberkeley.www.tedxberkeley.Views.TabsPagerAdapter;
 
 
-public class MainActivity extends ActionBarActivity implements SpeakerListFragment.OnFragmentInteractionListener {
+public class MainActivity extends ActionBarActivity implements SpeakerListFragment.OnFragmentInteractionListener,
+                                                               AboutFragment.OnFragmentInteractionListener,
+                                                               ScheduleFragment.OnFragmentInteractionListener{
 
     // Fragments
     private FragmentManager fragmentManager;
     private SpeakerListFragment mSpeakerListFragment;
+    private AboutFragment mAboutFragment;
+    private ScheduleFragment mScheduleFragment;
 
     // Navigation
     private ActionBar actionBar;
@@ -55,14 +62,27 @@ public class MainActivity extends ActionBarActivity implements SpeakerListFragme
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+        Log.e("Item Id", String.valueOf(id));
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        switch(id){
+            case R.id.schedule:
+                ft.replace(R.id.container, mScheduleFragment);
+            case R.id.speakers:
+                ft.replace(R.id.container, mSpeakerListFragment);
+            case R.id.about:
+                ft.replace(R.id.container, mAboutFragment);
+        }
+        ft.addToBackStack(null);
+        ft.commit();
         return super.onOptionsItemSelected(item);
     }
-
 
     public void onFragmentInteraction(Uri uri){}
 
     private void initializeFragments(){
         mSpeakerListFragment = SpeakerListFragment.newInstance();
+        mAboutFragment = AboutFragment.newInstance("Fight", "me");
+        mScheduleFragment = ScheduleFragment.newInstance("Schedule", "me");
         fragmentManager = getSupportFragmentManager();
         FragmentTransaction ft = fragmentManager.beginTransaction();
         ft.add(R.id.container, mSpeakerListFragment);
