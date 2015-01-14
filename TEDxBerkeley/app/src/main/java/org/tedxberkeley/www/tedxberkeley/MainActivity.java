2abd5a2484bfb98fa946cb35dd96c6cd5a0451bb
Max Wolffe <max.alan.wolffe@gmail.com>
2015-01-13 17:04:03 -0800

    ActionBar tabs deprecated in API 21, starting switch to SlidingTabs

--- TEDxBerkeley/app/src/main/java/org/tedxberkeley/www/tedxberkeley/MainActivity.java
+++ TEDxBerkeley/app/src/main/java/org/tedxberkeley/www/tedxberkeley/MainActivity.java
@@ -1,11 +1,14 @@
 package org.tedxberkeley.www.tedxberkeley;
 
-import android.app.ActionBar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.Tab;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.net.Uri;
@@ -15,7 +18,7 @@ import org.tedxberkeley.www.tedxberkeley.Speakers.SpeakerListFragment;
 import org.tedxberkeley.www.tedxberkeley.Views.TabsPagerAdapter;
 
 
-public class MainActivity extends FragmentActivity implements SpeakerListFragment.OnFragmentInteractionListener {
+public class MainActivity extends ActionBarActivity implements SpeakerListFragment.OnFragmentInteractionListener {
 
     // Fragments
     private FragmentManager fragmentManager;
@@ -32,7 +35,12 @@ public class MainActivity extends FragmentActivity implements SpeakerListFragmen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         initializeFragments();
+        initializeTabs(0);
     }
 
     @Override
@@ -44,22 +52,20 @@ public class MainActivity extends FragmentActivity implements SpeakerListFragmen
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
     public void initializeTabs(int option){
+        actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+            public void onTabSelected(Tab tab, FragmentTransaction ft) {
                 viewPager.setCurrentItem(tab.getPosition());
             }
-            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+            public void onTabUnselected(Tab tab, FragmentTransaction ft) {
                 // hide the given tab
             }
-            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+            public void onTabReselected(Tab tab, FragmentTransaction ft) {
                 // probably ignore this event
             }
         };
@@ -71,11 +77,15 @@ public class MainActivity extends FragmentActivity implements SpeakerListFragmen
         if (option == 0) {
             actionBar.addTab(
                     actionBar.newTab()
-                            .setText("Your Tasks")
+                            .setText("Schedule")
                             .setTabListener(tabListener));
             actionBar.addTab(
                     actionBar.newTab()
-                            .setText("All Tasks")
+                            .setText("Speakers")
+                            .setTabListener(tabListener));
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText("About")
                             .setTabListener(tabListener));
         }
 
@@ -95,8 +105,11 @@ public class MainActivity extends FragmentActivity implements SpeakerListFragmen
             @Override
             public void onPageScrollStateChanged(int arg0) {
             }
+
+
         });
     }
+
 
     public void onFragmentInteraction(Uri uri){}
 
